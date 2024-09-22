a74bb141a0f7f3579a29b618fa0930a3b252a851
MaverickPL07 <nanookmetaal52@gmail.com>
2024-09-22 14:18:03 +0200

    Mobile responisivity

--- src/components/DividerCircle.jsx
+++ src/components/DividerCircle.jsx
@@ -1,7 +1,7 @@
 const DividerCircle = () => {
     return (
         <>
-            <div className="w-[.5vw] h-[.5vw] border-[.1vw] border-[#323232] border-solid rounded-full bg-transparent"></div>
+            <div className="w-[.5vw] h-[.5vw] border-[1px] border-[#323232] border-solid rounded-full bg-transparent"></div>
         </>
     )
 }
